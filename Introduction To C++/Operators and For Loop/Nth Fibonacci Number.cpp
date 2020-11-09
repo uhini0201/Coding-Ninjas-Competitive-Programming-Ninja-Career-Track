0315221d5076de2902bea61c3a0de60ef4c2cf0d
Uhini Mukherjee <59994163+Uhini0201@users.noreply.github.com>
2020-11-09 19:46:19 +0530

    Create Nth Fibonacci Number.cpp

---
+++ Introduction To C++/Operators and For Loop/Nth Fibonacci Number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+   int n;
+    cin>>n;
+    if(n==1||n==2)
+    {    cout<<"1";return 0;}
+    int a=1,b=1,c;
+    for(int i=3;i<=n;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+    }
+    cout<<c;
+  
+}
