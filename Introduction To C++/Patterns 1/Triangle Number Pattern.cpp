f2b075847a23cceebf73e4465d9b68e2ea2ce44e
Uhini Mukherjee <59994163+Uhini0201@users.noreply.github.com>
2020-11-09 19:01:32 +0530

    Create Triangle Number Pattern.cpp

---
+++ Introduction To C++/Patterns 1/Triangle Number Pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+    int n , i , j;
+    cin >> n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            cout << i;
+        }
+        cout<<endl;
+    }
+  
+}
