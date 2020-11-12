c524901654222268268d455a5e7cbfd689ad02fe
Uhini Mukherjee <59994163+Uhini0201@users.noreply.github.com>
2020-11-12 21:59:12 +0530

    Create Find Unique.cpp

---
+++ Introduction To C++/Arrays/Find Unique.cpp
@@ -0,0 +1,23 @@
+
+int findUnique(int *a, int n)
+{ 
+    int maxi=-1;
+   for(int i=0;i<n;i++)
+   {
+      if(a[i]>maxi)
+          maxi=a[i];
+   }
+    
+    int m[maxi+1];
+    for(int i=0;i<=maxi;i++)
+        m[i]=0;
+        
+    for(int i=0;i<n;i++)
+    {
+        m[a[i]]++;
+        }
+    for(int i=0;i<n;i++){
+     if(m[a[i]]==1)
+            return a[i];
+    }
+}
