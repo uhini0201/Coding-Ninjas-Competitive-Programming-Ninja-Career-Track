09723bc7bd279bdf151410ab00a2ca62fafd9258
Deeptarshi Biswas <73191735+deepu0201@users.noreply.github.com>
2020-11-13 20:23:18 +0530

    Create Minimum Length Word.cpp

---
+++ Introduction To C++/Character Array And 2D Arrays/Minimum Length Word.cpp
@@ -0,0 +1,33 @@
+/* input - Input String
+*  output - Save the result in the output array (passed as argument). You don’t have to 
+*  print or return the result
+*/
+void minLengthWord(char s[], char o[]){
+		
+	int n=0;
+    while(s[n]!='\0')
+      n++;
+    s[n]=' ';
+    s[++n]='\0';
+    int mini=1000000,l=0,j=0;
+    for(int i=0;i<=n;i++)
+    {
+        if(s[i]==' ')
+        {
+            if(l<mini)
+            {
+                mini=l;
+                int t=0;
+                for(int k=j;k<i;k++)
+                    o[t++]=s[k];
+                o[l]='\0';
+            }
+            l=0;
+            j=i+1;
+        }
+        else
+        {
+            l++;
+        }
+    }
+}
